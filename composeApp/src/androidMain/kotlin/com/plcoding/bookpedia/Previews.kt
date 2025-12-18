9d9e8391ba06ba2aef71b7228f5e05b5474f9ff8
Manueedes <manuleedes@gmail.com>
2025-12-18 17:07:49 +0300

    ceated search bar

---
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/Previews.kt
@@ -0,0 +1,37 @@
+package com.plcoding.bookpedia
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.plcoding.bookpedia.book.presentation.book_list.components.BookSearchBar
+
+@Preview
+@Composable
+private fun BookSearchBarPreview() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(Color.White)
+    ) {
+
+        BookSearchBar(
+            searchQuery = "",
+            onSearchQueryChange = {},
+            onTimeSearch = {},
+            modifier = Modifier
+                .fillMaxWidth()
+        )
+    }
+}
+
+
+
+//
+//@Composable
+//fun MaterialTheme(content: @Composable () -> Unit) {
+//    TODO("Not yet implemented")
+//}
