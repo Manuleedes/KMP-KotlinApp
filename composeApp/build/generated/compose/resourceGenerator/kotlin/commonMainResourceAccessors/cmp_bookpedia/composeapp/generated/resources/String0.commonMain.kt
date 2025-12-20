74b4b650131050c80d89b2cf1b6c8d151a1992b1
Manueedes <manuleedes@gmail.com>
2025-12-20 14:10:29 +0300

    launch compose app on platforms using httpClientFactory with ktor

--- composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/cmp_bookpedia/composeapp/generated/resources/String0.commonMain.kt
+++ composeApp/build/generated/compose/resourceGenerator/kotlin/commonMainResourceAccessors/cmp_bookpedia/composeapp/generated/resources/String0.commonMain.kt
@@ -1,25 +1,208 @@
-@file:OptIn(InternalResourceApi::class)
+@file:OptIn(org.jetbrains.compose.resources.InternalResourceApi::class)
 
 package cmp_bookpedia.composeapp.generated.resources
 
 import kotlin.OptIn
+import kotlin.String
+import kotlin.collections.MutableMap
 import org.jetbrains.compose.resources.InternalResourceApi
 import org.jetbrains.compose.resources.StringResource
-import org.jetbrains.compose.resources.ResourceItem
-import org.jetbrains.compose.resources.LanguageQualifier
-import org.jetbrains.compose.resources.RegionQualifier
-import org.jetbrains.compose.resources.ThemeQualifier
-import org.jetbrains.compose.resources.DensityQualifier
 
-internal val Res.string.close_hint: StringResource by lazy { StringResource("string:close_hint", "close_hint", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.disk_full: StringResource by lazy { StringResource("string:disk_full", "disk_full", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.favorites: StringResource by lazy { StringResource("string:favorites", "favorites", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.no_books: StringResource by lazy { StringResource("string:no_books", "no_books", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.no_internet: StringResource by lazy { StringResource("string:no_internet", "no_internet", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.no_search: StringResource by lazy { StringResource("string:no_search", "no_search", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.request_timeout: StringResource by lazy { StringResource("string:request_timeout", "request_timeout", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.search_hint: StringResource by lazy { StringResource("string:search_hint", "search_hint", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.search_results: StringResource by lazy { StringResource("string:search_results", "search_results", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.serialization: StringResource by lazy { StringResource("string:serialization", "serialization", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.too_many_requests: StringResource by lazy { StringResource("string:too_many_requests", "too_many_requests", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
-internal val Res.string.unknown: StringResource by lazy { StringResource("string:unknown", "unknown", setOf(ResourceItem(setOf(),"values/strings.xml",-1,-1))) }
+private object CommonMainString0 {
+  public val close_hint: StringResource by 
+      lazy { init_close_hint() }
+
+  public val disk_full: StringResource by 
+      lazy { init_disk_full() }
+
+  public val favorites: StringResource by 
+      lazy { init_favorites() }
+
+  public val no_books: StringResource by 
+      lazy { init_no_books() }
+
+  public val no_internet: StringResource by 
+      lazy { init_no_internet() }
+
+  public val no_search: StringResource by 
+      lazy { init_no_search() }
+
+  public val request_timeout: StringResource by 
+      lazy { init_request_timeout() }
+
+  public val search_hint: StringResource by 
+      lazy { init_search_hint() }
+
+  public val search_results: StringResource by 
+      lazy { init_search_results() }
+
+  public val serialization: StringResource by 
+      lazy { init_serialization() }
+
+  public val too_many_requests: StringResource by 
+      lazy { init_too_many_requests() }
+
+  public val unknown: StringResource by 
+      lazy { init_unknown() }
+}
+
+@InternalResourceApi
+internal fun _collectCommonMainString0Resources(map: MutableMap<String, StringResource>) {
+  map.put("close_hint", CommonMainString0.close_hint)
+  map.put("disk_full", CommonMainString0.disk_full)
+  map.put("favorites", CommonMainString0.favorites)
+  map.put("no_books", CommonMainString0.no_books)
+  map.put("no_internet", CommonMainString0.no_internet)
+  map.put("no_search", CommonMainString0.no_search)
+  map.put("request_timeout", CommonMainString0.request_timeout)
+  map.put("search_hint", CommonMainString0.search_hint)
+  map.put("search_results", CommonMainString0.search_results)
+  map.put("serialization", CommonMainString0.serialization)
+  map.put("too_many_requests", CommonMainString0.too_many_requests)
+  map.put("unknown", CommonMainString0.unknown)
+}
+
+internal val Res.string.close_hint: StringResource
+  get() = CommonMainString0.close_hint
+
+private fun init_close_hint(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:close_hint", "close_hint",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    10, 38),
+    )
+)
+
+internal val Res.string.disk_full: StringResource
+  get() = CommonMainString0.disk_full
+
+private fun init_disk_full(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:disk_full", "disk_full",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    49, 49),
+    )
+)
+
+internal val Res.string.favorites: StringResource
+  get() = CommonMainString0.favorites
+
+private fun init_favorites(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:favorites", "favorites",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    99, 33),
+    )
+)
+
+internal val Res.string.no_books: StringResource
+  get() = CommonMainString0.no_books
+
+private fun init_no_books(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:no_books", "no_books",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    133, 68),
+    )
+)
+
+internal val Res.string.no_internet: StringResource
+  get() = CommonMainString0.no_internet
+
+private fun init_no_internet(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:no_internet", "no_internet",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    202, 91),
+    )
+)
+
+internal val Res.string.no_search: StringResource
+  get() = CommonMainString0.no_search
+
+private fun init_no_search(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:no_search", "no_search",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    294, 57),
+    )
+)
+
+internal val Res.string.request_timeout: StringResource
+  get() = CommonMainString0.request_timeout
+
+private fun init_request_timeout(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:request_timeout", "request_timeout",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    352, 55),
+    )
+)
+
+internal val Res.string.search_hint: StringResource
+  get() = CommonMainString0.search_hint
+
+private fun init_search_hint(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:search_hint", "search_hint",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    408, 35),
+    )
+)
+
+internal val Res.string.search_results: StringResource
+  get() = CommonMainString0.search_results
+
+private fun init_search_results(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:search_results", "search_results",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    444, 42),
+    )
+)
+
+internal val Res.string.serialization: StringResource
+  get() = CommonMainString0.serialization
+
+private fun init_serialization(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:serialization", "serialization",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    487, 57),
+    )
+)
+
+internal val Res.string.too_many_requests: StringResource
+  get() = CommonMainString0.too_many_requests
+
+private fun init_too_many_requests(): StringResource =
+    org.jetbrains.compose.resources.StringResource(
+  "string:too_many_requests", "too_many_requests",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    545, 61),
+    )
+)
+
+internal val Res.string.unknown: StringResource
+  get() = CommonMainString0.unknown
+
+private fun init_unknown(): StringResource = org.jetbrains.compose.resources.StringResource(
+  "string:unknown", "unknown",
+    setOf(
+      org.jetbrains.compose.resources.ResourceItem(setOf(),
+    "composeResources/cmp_bookpedia.composeapp.generated.resources/values/strings.commonMain.cvr",
+    607, 47),
+    )
+)
