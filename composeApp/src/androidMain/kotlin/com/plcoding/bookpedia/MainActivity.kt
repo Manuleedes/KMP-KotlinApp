f5979d6a18a0607f1c50b2a726fdb69c3675596f
Manueedes <manuleedes@gmail.com>
2025-12-22 12:25:12 +0300

    implemented navigation graph to enable switch to details screen

--- composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/plcoding/bookpedia/MainActivity.kt
@@ -3,7 +3,9 @@ package com.plcoding.bookpedia
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.plcoding.bookpedia.app.App
 
+//navigation library integration
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
